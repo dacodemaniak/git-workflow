be9b23f7bd286a9829b3687a82e28e4228bff52e
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-04-21 14:12:48 +0200

    Add some CSS to customize form Add loader function after document was loaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-document.querySelector('h1').textContent = 'Git Flow'
+/**
+ * main.js
+ * Application loader
+ */
+
+(() => {
+    console.log(`Index was loaded`)
+    document.querySelector('h1').textContent = 'Git Flow'
+})()
